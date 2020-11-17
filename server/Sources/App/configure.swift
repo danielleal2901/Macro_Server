66c9a7868fc36f37bef4bf7698ca00035991d1a6
Antonio Carlos <antonio.correa0407@gmail.com>
2020-11-17 09:24:16 -0300

    Arrumando o configure.swift

--- server/Sources/App/configure.swift
+++ server/Sources/App/configure.swift
@@ -11,11 +11,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    app.databases.use(.postgres(
-        hostname: "localhost",
-        username: "postgres",
-        password: "",
-        database: "macro_challenge_dev"), as: .psql)
+    app.databases.use(try .postgres(url: Environment.get("DATABASE_URL") ?? "postgres://postgres@localhost:5432/macro_challenge_dev"), as: .psql)
     
     app.http.server.configuration.hostname = "127.0.0.1"    
     app.http.server.configuration.port = 8080
@@ -24,14 +20,12 @@ public func configure(_ app: Application) throws {
     app.sessions.use(.memory)
     
     // Mailer
-
-    app.mailgun.configuration = .init(apiKey: "2b7a87ba6bae7abe21d5fe1e1faf56ad-ea44b6dc-cd9d4697")
+    app.mailgun.configuration = MailgunConfiguration(apiKey: Environment.get("MAILGUN_API_KEY") ?? "2b7a87ba6bae7abe21d5fe1e1faf56ad-ea44b6dc-cd9d4697")
     app.mailgun.defaultDomain = .mainDomain
         
     //Enums: should be called first
-//    app.migrations.add(CreateStageTypesEnum())
-//    app.migrations.add(CreateStagesContainersTypesEnum())
-
+    app.migrations.add(CreateStageTypesEnum())
+    app.migrations.add(CreateStagesContainersTypesEnum())
     app.migrations.add(CreateTeam())
     app.migrations.add(CreateFarm())
     app.migrations.add(CreateStagesContainer())
